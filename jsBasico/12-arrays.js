e1092d85569b4242e81a2060a4dc6c9e0b6bcb18
alejorminetti <alejorm@gmail.com>
2021-10-08 20:09:33 -0300

    Ampliando arrays

--- jsBasico/12-arrays.js
+++ jsBasico/12-arrays.js
@@ -1,42 +1,28 @@
+// https://www.w3schools.com/js/js_arrays.asp
 // Un array es una estructura de datos, es un objeto tipo lista de alto nivel.
 
 // Los arrays son objetos de tipo lista cuyo prototipo tiene métodos para realizar operaciones de recorrido y mutación
 
-var frutas = []; // Array Literal Syntax 
+var frutas = []; // Array Literal Syntax
 
-var frutas = ["Manzana", "Platano", "Cereza", "Fresa"]; 
-console.log(frutas); 
+var frutas = ["Manzana", "Platano", "Cereza", "Fresa"];
+console.log(frutas);
 console.log(frutas.length); // length es una propiedad del array
 
 // Acceder (por índice) a un elemento del Array
-console.log(frutas[0]);   // Los arrays iician en "0" 
+console.log(frutas[0]); // Los arrays iician en "0"
 
+//to String
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
 
-// === Metodos para mutar arrays ===
-
-
-// .push();
-
- var masFrutas = frutas.push("Uvas"); // Esté metodo añadirá "Uvas" añ final del array
- console.log(frutas);
-
-// .pop();
-
- var utlimo = frutas.pop("Uvas"); // Eliminará "Uvas" del final 
- console.log(frutas);
-
-// unshift()
-
-var nuevaLogitud = frutas.unshift("Uvas"); // Añade "Uvas" al inicio 
-console.log(frutas);
-
-// shift()
-
-var borrarFruta = frutas.shift("Manzana"); // Elimina "Manzana" del inico
-console.log(frutas);
-
-// .indexOf();
-
-var posicion = frutas.indexOf("Platano"); // te dará la posición de ese item en el array
-console.log(frutas);
-
+// loop en array multidimencional (snippet: marrLoop)
+for (let fila = 0; fila < array.length; fila++) {
+  if (array[fila].length) {
+    for (let colum = 0; colum < array[0].length; colum++) {
+      array[fila][colum] += 1;
+    }
+  } else {
+    array[fila] += 1;
+  }
+}
--- jsBasico/13-arr-methods.js
+++ jsBasico/13-arr-methods.js
@@ -8,9 +8,7 @@ var articulos = [
     { nombre: 'audifonos', costo: 1700 }
 ]
 
-
 // Metodos para recorrer arrays 
-
 
 //   Metodo Filter  
 /* Válida si es un true o false para poder meterlos al nuevo array, y éste método no modifica el array original */
@@ -80,3 +78,71 @@ var incluyeNumero = numeros.includes(2);
 
 console.log(incluyeNumero);
 
+// === Metodos para mutar arrays ===
+
+// .push();
+
+var masFrutas = frutas.push("Uvas"); // Esté metodo añadirá "Uvas" añ final del array
+console.log(frutas);
+frutas[frutas.length] = "Uvas"; //equivalent to push
+
+// .pop();
+
+var utlimo = frutas.pop("Uvas"); // Eliminará "Uvas" del final
+console.log(frutas);
+
+// unshift()
+
+var nuevaLogitud = frutas.unshift("Uvas"); // Añade "Uvas" al inicio
+console.log(frutas);
+
+// shift()
+
+var borrarFruta = frutas.shift("Manzana"); // Elimina "Manzana" del inico
+console.log(frutas);
+
+// .indexOf();
+
+var posicion = frutas.indexOf("Platano"); // te dará la posición de ese item en el array
+console.log(frutas);
+
+//joins()
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.join(" * ");
+
+//delete
+delete fruits[0]; // Changes the first element in fruits to undefined
+
+//splice
+// The first parameter (2) defines the position where new elements should be added (spliced in).
+// The second parameter (0) defines how many elements should be removed.
+// The rest of the parameters ("Lemon" , "Kiwi") define the new elements to be added.
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+fruits.splice(0, 1); // Removes the first element
+
+///// for New arrays
+
+//concat
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+// Concatenate (join) myGirls and myBoys
+const myChildren = myGirls.concat(myBoys);
+// triple concat
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2, arr3);
+
+//slice
+// The slice() method creates a new array. It does not remove any elements from the source array.
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+
+// Loops de arrays
+
+// forEach
+var StringeandoElArray = "";
+array.forEach((element) => {
+  StringeandoElArray += element;
+});
