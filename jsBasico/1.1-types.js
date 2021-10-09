19b6c464800d606ffd158110b788cf794846fa8b
Alejo R Minetti <alejorm@gmail.com>
2021-10-09 12:21:51 -0300

    conversion de datos y concepto de coercion

--- jsBasico/1.1-types.js
+++ jsBasico/1.1-types.js
@@ -18,7 +18,14 @@ typeof null
 typeof {}
 typeof []
 
-// conversion de datos
+//ver tipo de dato:
+typeof "hola"
+
+// conversion de datos (coercion)
+String(10)
+Number("23")
+// al hacer operaciones puede suceder coercion implicita: 4 + "7" -> "47" o 2 * "3" -> 6
+// si lo forzamos mediante conversion con estos metodos: es explicita
 
 // int to string. base=10 decimal. base = 2 binario
 number.toString(base)
