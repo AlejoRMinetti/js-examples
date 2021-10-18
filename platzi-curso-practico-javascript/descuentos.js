67297c9aa98f0f2aa22ce856445aabf7671d2ef6
Alejo R Minetti <alejorm@gmail.com>
2021-10-18 14:34:36 -0300

    pagina descuentos

--- platzi-curso-practico-javascript/descuentos.js
+++ platzi-curso-practico-javascript/descuentos.js
@@ -1,6 +1,8 @@
 // const precioOriginal = 120;
 // const descuento = 18;
 
+// cupones de descuento
+var cupones = ["25%", "30%", "25%"];
 
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
@@ -12,21 +14,37 @@ function calcularPrecioConDescuento(precio, descuento) {
 function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
-  
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
 
+  let descuento;
   const resultP = document.getElementById("ResultP");
+
+  switch (couponValue) {
+    case coupons[0]:
+      descuento = 15;
+      break;
+    case coupons[1]:
+      descuento = 30;
+      break;
+    case coupons[2]:
+      descuento = 25;
+      break;
+    default:
+      resultP.innerText = "Cupong no valido!";
+      break;
+  }
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
   resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
 
-
+// Para imprimir un objeto con multiples valores (util para debug)
 // console.log({
 //   precioOriginal,
 //   descuento,
 //   porcentajePrecioConDescuento,
 //   precioConDescuento,
 // });
-
