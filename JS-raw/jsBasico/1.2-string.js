2cf4f96ce8c6ba9570c34124ec845c487f4c64f9
CHAMATRONIC <alejorm@gmail.com>
2023-01-20 12:02:42 -0300

    intro React with ToDoList added as submodule of forked repo

--- JS-raw/jsBasico/1.2-string.js
+++ JS-raw/jsBasico/1.2-string.js
@@ -3,6 +3,9 @@
 var str = 'AppDividend';
 console.log('Original String: ', str); //Original String:  AppDividend
 
+// include
+console.log('Include Div String: ', str.includes('Div')); //Include Div String: AppDividend
+
 // replace
 newStr = str.replace(/D/g, '');
 console.log('After character removed: ', newStr); 
@@ -12,27 +15,37 @@ console.log('After character removed: ', newStr);
 // only the first instance of the value will be replaced. 
 // To replace all occurrences of a specified value, use 
 // the global (g) modifier 
+{
+  // replace() does not change the original string.
+  let str = "Mr Blue has a blue house and a blue car";
+  str.replace(/blue|house|car/gi, function (x) {
+    return x.toUpperCase();
+  });
+  // Mr BLUE has a BLUE HOUSE and a BLUE CAR.
+}
 
-// replace() does not change the original string.
-let str = "Mr Blue has a blue house and a blue car";
-str.replace(/blue|house|car/gi, function (x) {
-  return x.toUpperCase();
-});
-// Mr BLUE has a BLUE HOUSE and a BLUE CAR.
+{
+  // slice
+  var str = 'AppDividend';
+  removeFirstChar = str.slice(1);
+  console.log('Removing the first character with slice', removeFirstChar); // ppDividend
+  removeLastChar = str.slice(0, str.length - 1);
+  console.log('Removing the last character: ', removeLastChar); // AppDividen
+}
 
-// slice
-var str = 'AppDividend';
-removeFirstChar = str.slice(1);
-console.log('Removing the first character', removeFirstChar); // ppDividend
-removeLastChar = str.slice(0, str.length - 1);
-console.log('Removing the last character: ', removeLastChar); // AppDividen
+{
+  // substr
+  var str = 'AppDividend';
+  newStr = str.substr(1, str.length);
+  console.log('Removing the first character with substr:', newStr); // ppDividend
 
-// substr
-var str = 'AppDividend';
-newStr = str.substr(1, str.length);
-console.log('After removing the first character:', newStr); // ppDividend
+}
 
-// https://www.w3schools.com/jsref/jsref_split.asp#:~:text=JavaScript%20String%20split%20%28%29%20Method%201%20Definition%20and,the%20string.%205%20Technical%20Details%206%20More%20Examples
-// Split
-let str = "How are you doing today?";
-const myArr = str.split(" ");
+{
+  // https://www.w3schools.com/jsref/jsref_split.asp#:~:text=JavaScript%20String%20split%20%28%29%20Method%201%20Definition%20and,the%20string.%205%20Technical%20Details%206%20More%20Examples
+  // Split
+  let str = "How are you doing today?";
+  const myArr = str.split(" ");
+  console.log(myArr)
+}
+
